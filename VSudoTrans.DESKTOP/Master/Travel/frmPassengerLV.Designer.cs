ce26adaff3944dfe9a07d6913c706b6ddbb2c89d
ikhsan.maulana <ikhsan.maulana@bumitama.com>
2024-10-28 00:08:41 +0700

    feat: add Passenger

---
+++ VSudoTrans.DESKTOP/Master/Travel/frmPassengerLV.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace VSudoTrans.DESKTOP.Master.Travel
+{
+    partial class frmPassengerLV
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPassengerLV));
+            this.colCompanyName = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colCode = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colName = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colPhoneNumber = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colEmail = new DevExpress.XtraGrid.Columns.GridColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.ribbonControl)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataLayoutControl1)).BeginInit();
+            this.dataLayoutControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this._GridControl)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this._GridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Root)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ItemForGridControl)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this._SearchControl.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this._DockManager)).BeginInit();
+            this._DockPanelRight.SuspendLayout();
+            this.dockPanel1_Container.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataLayoutControl2)).BeginInit();
+            this.dataLayoutControl2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.FilterDate1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FilterDate1.Properties.CalendarTimeProperties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this._LayoutControlItemFilter1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FilterDate2.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FilterDate2.Properties.CalendarTimeProperties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this._LayoutControlItemFilter2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this._LayoutControlItemFilter3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this._BindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FilterPopUp3.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this._DxValidationProvider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ribbonControl
+            // 
+            this.ribbonControl.ExpandCollapseItem.Id = 0;
+            this.ribbonControl.Size = new System.Drawing.Size(971, 254);
+            // 
+            // bbiRefresh
+            // 
+            this.bbiRefresh.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("bbiRefresh.ImageOptions.SvgImage")));
+            // 
+            // bbiNew
+            // 
+            this.bbiNew.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("bbiNew.ImageOptions.SvgImage")));
+            // 
+            // bbiEdit
+            // 
+            this.bbiEdit.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("bbiEdit.ImageOptions.SvgImage")));
+            // 
+            // bbiDelete
+            // 
+            this.bbiDelete.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("bbiDelete.ImageOptions.SvgImage")));
+            // 
+            // bbiCopy
+            // 
+            this.bbiCopy.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("bbiCopy.ImageOptions.SvgImage")));
+            // 
+            // bbiPrintPreview
+            // 
+            this.bbiPrintPreview.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("bbiPrintPreview.ImageOptions.SvgImage")));
+            // 
+            // bbiExport
+            // 
+            this.bbiExport.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("bbiExport.ImageOptions.SvgImage")));
+            // 
+            // bbiExportCsv
+            // 
+            this.bbiExportCsv.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("bbiExportCsv.ImageOptions.SvgImage")));
+            // 
+            // bbiExportPdf
+            // 
+            this.bbiExportPdf.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("bbiExportPdf.ImageOptions.SvgImage")));
+            // 
+            // bbiExportHtml
+            // 
+            this.bbiExportHtml.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("bbiExportHtml.ImageOptions.SvgImage")));
+            // 
+            // bbiExportXls
+            // 
+            this.bbiExportXls.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("bbiExportXls.ImageOptions.SvgImage")));
+            // 
+            // bbiExportXlsx
+            // 
+            this.bbiExportXlsx.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("bbiExportXlsx.ImageOptions.SvgImage")));
+            // 
+            // bbiExportDoc
+            // 
+            this.bbiExportDoc.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("bbiExportDoc.ImageOptions.SvgImage")));
+            // 
+            // bbiExportDocX
+            // 
+            this.bbiExportDocX.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("bbiExportDocX.ImageOptions.SvgImage")));
+            // 
+            // bbiClose
+            // 
+            this.bbiClose.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("bbiClose.ImageOptions.SvgImage")));
+            // 
+            // dataLayoutControl1
+            // 
+            this.dataLayoutControl1.Location = new System.Drawing.Point(0, 254);
+            this.dataLayoutControl1.Size = new System.Drawing.Size(649, 424);
+            this.dataLayoutControl1.Controls.SetChildIndex(this._GridControl, 0);
+            this.dataLayoutControl1.Controls.SetChildIndex(this._SearchControl, 0);
+            // 
+            // _GridControl
+            // 
+            this._GridControl.DataSource = this._BindingSource;
+            this._GridControl.EmbeddedNavigator.Buttons.Append.Visible = false;
+            this._GridControl.EmbeddedNavigator.Buttons.CancelEdit.Visible = false;
+            this._GridControl.EmbeddedNavigator.Buttons.Edit.Visible = false;
+            this._GridControl.EmbeddedNavigator.Buttons.EndEdit.Visible = false;
+            this._GridControl.EmbeddedNavigator.Buttons.First.Visible = false;
+            this._GridControl.EmbeddedNavigator.Buttons.Last.Visible = false;
+            this._GridControl.EmbeddedNavigator.Buttons.Remove.Visible = false;
+            this._GridControl.Size = new System.Drawing.Size(627, 338);
+            // 
+            // _GridView
+            // 
+            this._GridView.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.colCompanyName,
+            this.colCode,
+            this.colName,
+            this.colPhoneNumber,
+            this.colEmail});
+            this._GridView.OptionsBehavior.Editable = false;
+            // 
+            // Root
+            // 
+            this.Root.Size = new System.Drawing.Size(649, 424);
+            // 
+            // ItemForGridControl
+            // 
+            this.ItemForGridControl.Size = new System.Drawing.Size(631, 342);
+            // 
+            // _SearchControl
+            // 
+            this._SearchControl.Size = new System.Drawing.Size(627, 34);
+            // 
+            // layoutControlItem2
+            // 
+            this.layoutControlItem2.Size = new System.Drawing.Size(631, 64);
+            // 
+            // _DockPanelRight
+            // 
+            this._DockPanelRight.Location = new System.Drawing.Point(649, 254);
+            this._DockPanelRight.Size = new System.Drawing.Size(322, 424);
+            // 
+            // dockPanel1_Container
+            // 
+            this.dockPanel1_Container.Size = new System.Drawing.Size(311, 373);
+            // 
+            // dataLayoutControl2
+            // 
+            this.dataLayoutControl2.Size = new System.Drawing.Size(311, 373);
+            this.dataLayoutControl2.Controls.SetChildIndex(this.FilterPopUp3, 0);
+            this.dataLayoutControl2.Controls.SetChildIndex(this.FilterDate1, 0);
+            this.dataLayoutControl2.Controls.SetChildIndex(this.FilterDate2, 0);
+            // 
+            // FilterDate1
+            // 
+            this.FilterDate1.EditValue = null;
+            // 
+            // layoutControlGroup1
+            // 
+            this.layoutControlGroup1.Size = new System.Drawing.Size(311, 373);
+            // 
+            // emptySpaceItem1
+            // 
+            this.emptySpaceItem1.Size = new System.Drawing.Size(293, 163);
+            // 
+            // FilterDate2
+            // 
+            this.FilterDate2.EditValue = null;
+            // 
+            // _BindingSource
+            // 
+            this._BindingSource.DataSource = typeof(Domain.Entities.Travel.Passenger);
+            // 
+            // FilterPopUp3
+            // 
+            this.FilterPopUp3.Properties.Appearance.Options.UseTextOptions = true;
+            this.FilterPopUp3.Properties.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Near;
+            // 
+            // bbiImport
+            // 
+            this.bbiImport.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("bbiImport.ImageOptions.SvgImage")));
+            // 
+            // bbiTemplateImport
+            // 
+            this.bbiTemplateImport.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("bbiTemplateImport.ImageOptions.SvgImage")));
+            // 
+            // bbiImportData
+            // 
+            this.bbiImportData.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("bbiImportData.ImageOptions.SvgImage")));
+            // 
+            // colCompanyName
+            // 
+            this.colCompanyName.Caption = "Perusahaan";
+            this.colCompanyName.FieldName = "Company.Name";
+            this.colCompanyName.MinWidth = 30;
+            this.colCompanyName.Name = "colCompanyName";
+            this.colCompanyName.Visible = true;
+            this.colCompanyName.VisibleIndex = 0;
+            this.colCompanyName.Width = 259;
+            // 
+            // colCode
+            // 
+            this.colCode.Caption = "Kode";
+            this.colCode.FieldName = "Code";
+            this.colCode.MinWidth = 30;
+            this.colCode.Name = "colCode";
+            this.colCode.Visible = true;
+            this.colCode.VisibleIndex = 1;
+            this.colCode.Width = 186;
+            // 
+            // colName
+            // 
+            this.colName.Caption = "Nama";
+            this.colName.FieldName = "Name";
+            this.colName.MinWidth = 30;
+            this.colName.Name = "colName";
+            this.colName.Visible = true;
+            this.colName.VisibleIndex = 2;
+            this.colName.Width = 370;
+            // 
+            // colPhoneNumber
+            // 
+            this.colPhoneNumber.Caption = "Nomor Telepon";
+            this.colPhoneNumber.FieldName = "PhoneNumber";
+            this.colPhoneNumber.MinWidth = 30;
+            this.colPhoneNumber.Name = "colPhoneNumber";
+            this.colPhoneNumber.Visible = true;
+            this.colPhoneNumber.VisibleIndex = 3;
+            this.colPhoneNumber.Width = 325;
+            // 
+            // colEmail
+            // 
+            this.colEmail.Caption = "Email";
+            this.colEmail.FieldName = "Email";
+            this.colEmail.MinWidth = 30;
+            this.colEmail.Name = "colEmail";
+            this.colEmail.Visible = true;
+            this.colEmail.VisibleIndex = 4;
+            this.colEmail.Width = 336;
+            // 
+            // frmPassengerLV
+            // 
+            this.Appearance.Options.UseFont = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(971, 678);
+            this.Name = "frmPassengerLV";
+            this.Text = "frmPassengerLV";
+            ((System.ComponentModel.ISupportInitialize)(this.ribbonControl)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataLayoutControl1)).EndInit();
+            this.dataLayoutControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this._GridControl)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this._GridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Root)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ItemForGridControl)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this._SearchControl.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this._DockManager)).EndInit();
+            this._DockPanelRight.ResumeLayout(false);
+            this.dockPanel1_Container.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataLayoutControl2)).EndInit();
+            this.dataLayoutControl2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.FilterDate1.Properties.CalendarTimeProperties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FilterDate1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this._LayoutControlItemFilter1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FilterDate2.Properties.CalendarTimeProperties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FilterDate2.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this._LayoutControlItemFilter2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this._LayoutControlItemFilter3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this._BindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FilterPopUp3.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this._DxValidationProvider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraGrid.Columns.GridColumn colCompanyName;
+        private DevExpress.XtraGrid.Columns.GridColumn colCode;
+        private DevExpress.XtraGrid.Columns.GridColumn colName;
+        private DevExpress.XtraGrid.Columns.GridColumn colPhoneNumber;
+        private DevExpress.XtraGrid.Columns.GridColumn colEmail;
+    }
+}
---
+++ VSudoTrans.DESKTOP/Master/Travel/frmPassengerLV.cs
@@ -0,0 +1,70 @@
+﻿using DevExpress.XtraEditors.DXErrorProvider;
+using Domain.Entities.Travel;
+using PopUpUtils;
+using VSudoTrans.DESKTOP.BaseForm;
+using VSudoTrans.DESKTOP.Utils;
+
+namespace VSudoTrans.DESKTOP.Master.Travel
+{
+    public partial class frmPassengerLV : frmBaseFilterLV
+    {
+        public frmPassengerLV()
+        {
+            InitializeComponent();
+
+            this.EndPoint = "/Passengers";
+            this.FormTitle = "Penumpang";
+
+            this.OdataSelect = "Id,PhoneNumber,Email,Code,Name";
+            this.OdataExpand = "Company($select=code,name)";
+
+            InitializeComponentAfter<Passenger>();
+
+            InitializeSearchLookup();
+
+            bbiRefresh.ItemClick += BbiRefresh_ItemClick;
+            bbiDelete.ItemClick += BbiDelete_ItemClick;
+        }
+
+        private void BbiDelete_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            ActionDelete<Passenger>();
+        }
+
+        private void BbiRefresh_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            ActionRefresh<Passenger>();
+        }
+
+        protected override void ActionShowFormDetail(object fCopy = null)
+        {
+            FormDetail = new frmPassengerDV(this.EntityId, this.EndPoint, fCopy);
+            base.ActionShowFormDetail();
+        }
+        protected override void InitializeDefaultValidation()
+        {
+            MyValidationHelper.SetValidation(_DxValidationProvider, this.FilterPopUp3, ConditionOperator.IsNotBlank);
+        }
+
+        protected override void InitializeSearchLookup()
+        {
+            _LayoutControlItemFilter1.Visibility = DevExpress.XtraLayout.Utils.LayoutVisibility.Never;
+            _LayoutControlItemFilter2.Visibility = DevExpress.XtraLayout.Utils.LayoutVisibility.Never;
+
+            _LayoutControlItemFilter3.Visibility = DevExpress.XtraLayout.Utils.LayoutVisibility.Always;
+            _LayoutControlItemFilter3.Text = "Perusahaan";
+            PopupEditHelper.Company(FilterPopUp3);
+        }
+
+        protected override void ActionRefresh<T>(string endPoint = "")
+        {
+            InitializeDefaultValidation();
+            if (!ActionValidate())
+                return;
+
+            this.OdataFilter = $"CompanyId eq {HelperConvert.Int(AssemblyHelper.GetValueProperty(FilterPopUp3.EditValue, "Id"))} ";
+
+            base.ActionRefresh<T>(endPoint);
+        }
+    }
+}
